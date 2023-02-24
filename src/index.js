dbdaf74909cb70ec521c0f2436a1146280fb2b77
Anjana Dunukara <anjanaheshanthach@gmail.com>
2023-02-24 16:54:23 +0530

    setup socket.io

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 const express = require("express");
+const http = require("http");
 const path = require("path");
+const socketio = require("socket.io");
 
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
 const port = process.env.PORT || 3000;
 const publicDirectoryPath = path.join(__dirname, "../public");
@@ -9,6 +13,10 @@ const publicDirectoryPath = path.join(__dirname, "../public");
 //Setup static directory to serve
 app.use(express.static(publicDirectoryPath));
 
-app.listen(port, () => {
+io.on("connection", () => {
+  console.log("New WebSocket Connection");
+});
+
+server.listen(port, () => {
   console.log(`Server is up and running on port ${port}!`);
 });
