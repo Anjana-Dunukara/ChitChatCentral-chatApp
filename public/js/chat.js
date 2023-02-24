a0a1f71889d91c0b7197bf0f6c6392949e417fe8
Anjana Dunukara <anjanaheshanthach@gmail.com>
2023-02-24 22:30:36 +0530

    Add Form and Button states

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,28 +1,58 @@
 const socket = io();
 
+//Elements
+const $messageForm = document.querySelector("#message-form");
+const $messageFormInput = $messageForm.querySelector("input");
+const $messageFormButton = $messageForm.querySelector("button");
+const $locationSendButton = document.querySelector("#send-location");
+
 socket.on("welcomemessage", (welcome) => {
   console.log(welcome);
 });
 
-const messageForm = document.querySelector("form");
 //const text = document.querySelector("input");
 
-messageForm.addEventListener("submit", (event) => {
+$messageForm.addEventListener("submit", (event) => {
   event.preventDefault();
+
+  //disable
+  $messageFormButton.setAttribute("disabled", "disabled");
+
   const message = event.target.elements.message.value;
-  socket.emit("messageSend", message);
+
+  socket.emit("messageSend", message, (error) => {
+    //enable
+    $messageFormButton.removeAttribute("disabled");
+    $messageFormInput.value = "";
+    $messageFormInput.focus();
+    if (error) {
+      return console.log(error);
+    }
+    console.log("Message Deliverd!");
+  });
 });
 
 socket.on("message", (message) => {
   console.log(`Bot: ${message}`);
 });
 
-document.querySelector("#send-location").addEventListener("click", () => {
+$locationSendButton.addEventListener("click", () => {
   if (!navigator.geolocation) {
     return alert("Geolocation is not supported by your browser..");
   }
 
+  //Disable
+  $locationSendButton.setAttribute("disabled", "disabled");
+
   navigator.geolocation.getCurrentPosition((position) => {
-    console.log(position);
+    const location = {
+      latitude: position.coords.latitude,
+      longitude: position.coords.longitude,
+    };
+    socket.emit("sendLocation", location, () => {
+      //enable
+      $locationSendButton.removeAttribute("disabled");
+      console.log("location shared!");
+    });
   });
 });
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const http = require("http");
 const path = require("path");
 const socketio = require("socket.io");
+const Filter = require("bad-words");
 
 const app = express();
 const server = http.createServer(app);
@@ -19,12 +20,27 @@ io.on("connection", (socket) => {
 
   socket.emit("welcomemessage", "Welcome!!");
 
-  socket.on("messageSend", (message) => {
+  socket.on("messageSend", (message, callback) => {
+    const filter = new Filter();
+
+    if (filter.isProfane(message)) {
+      return callback("Profanity is not allowed!!");
+    }
+
     io.emit("message", message);
+    callback();
   });
 
   socket.on("disconnect", () => {
     io.emit("message", "A User has left the chat..!!");
+  });
+
+  socket.on("sendLocation", (location, callback) => {
+    io.emit(
+      "message",
+      `https://google.com/maps?q=${location.latitude},${location.longitude}`
+    );
+    callback();
   });
 });
 
