a532ee4559a813997b5f6717f4d3267ac52d63b1
Anjana Dunukara <anjanaheshanthach@gmail.com>
2023-02-24 21:24:52 +0530

    add mdn geolocation to share your location

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,1 +1,28 @@
-io();
+const socket = io();
+
+socket.on("welcomemessage", (welcome) => {
+  console.log(welcome);
+});
+
+const messageForm = document.querySelector("form");
+//const text = document.querySelector("input");
+
+messageForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const message = event.target.elements.message.value;
+  socket.emit("messageSend", message);
+});
+
+socket.on("message", (message) => {
+  console.log(`Bot: ${message}`);
+});
+
+document.querySelector("#send-location").addEventListener("click", () => {
+  if (!navigator.geolocation) {
+    return alert("Geolocation is not supported by your browser..");
+  }
+
+  navigator.geolocation.getCurrentPosition((position) => {
+    console.log(position);
+  });
+});
--- src/index.js
+++ src/index.js
@@ -13,8 +13,19 @@ const publicDirectoryPath = path.join(__dirname, "../public");
 //Setup static directory to serve
 app.use(express.static(publicDirectoryPath));
 
-io.on("connection", () => {
+io.on("connection", (socket) => {
   console.log("New WebSocket Connection");
+  socket.broadcast.emit("message", "A New User has joined.!");
+
+  socket.emit("welcomemessage", "Welcome!!");
+
+  socket.on("messageSend", (message) => {
+    io.emit("message", message);
+  });
+
+  socket.on("disconnect", () => {
+    io.emit("message", "A User has left the chat..!!");
+  });
 });
 
 server.listen(port, () => {
